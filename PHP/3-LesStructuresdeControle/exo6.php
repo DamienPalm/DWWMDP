a10228d9a4c0a29b9967c7665ad12dc0ad21ac61
DamienPalm <palm.damien@icloud.com>
2022-11-02 16:17:07 +0100

    maj 02/11/22

---
+++ PHP/3-LesStructuresdeControle/exo6.php
@@ -0,0 +1,24 @@
+<?php
+
+$heure = readline("heure");
+$minute = readline("minute");
+
+$minute = $minute + 1;
+if ($minute == 60){
+    $minute = 0;
+    $heure = $heure + 1;
+    if ($heure >= 24){
+        $heure = $heure - 24;
+    }
+}
+
+if ($minute > 60){
+    $minute = $minute - 60;
+    $heure = $heure + 1;
+    if ($heure >= 24){
+        $heure = $heure - 24;
+    }
+}
+echo $heure . "h". $minute
+
+?>
