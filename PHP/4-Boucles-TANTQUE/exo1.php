44cc25c23922ecb5e35ae900371b24a29c56782b
DamienPalm <palm.damien@icloud.com>
2022-11-03 16:17:30 +0100

    maj 03/11/22

---
+++ PHP/4-Boucles-TANTQUE/exo1.php
@@ -0,0 +1,11 @@
+<?php
+
+$nb = readline ("mettre un nombre entre 1 et 3 : ");
+
+while ($nb < 1 || $nb > 3){
+    $nb = readline ("mettre un nombre entre 1 et 3 : ");
+}
+
+echo "GG";
+
+?>
---
+++ PHP/4-Boucles-TANTQUE/exoBonus2.php
@@ -0,0 +1,12 @@
+<?php
+
+$nb = readline("mettre un nombre ");
+$compteur = 0;
+$factorielle;
+
+while ($compteur != $nb){
+    $compteur = $compteur + 1;
+    echo $nb*$compteur . "\n";
+}
+
+?>
