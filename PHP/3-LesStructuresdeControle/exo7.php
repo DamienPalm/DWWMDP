d014aefe72ebd34200c52d96e19752ec9a7ef87a
DamienPalm <palm.damien@icloud.com>
2022-11-03 09:56:20 +0100

    maj 03/11/22

--- PHP/3-LesStructuresdeControle/exo7.php
+++ PHP/3-LesStructuresdeControle/exo7.php
@@ -8,14 +8,8 @@ $seconde = $seconde + 1;
 if ($seconde == 60){
     $seconde = 0;
     $minute = $minute + 1;
-    if ($minute > 60){
-        $minute = $minute - 60;
-    }
 }
 
-if ($heure >= 24){
-    $heure = $heure - 24;
-}
 
 if ($seconde > 60){
     $seconde = $seconde - 60;
@@ -25,13 +19,16 @@ if ($seconde > 60){
 if ($minute == 60){
     $minute = 0;
     $heure = $heure + 1;
-    if ($minute > 60){
-        $minute = $minute - 60;
-        $heure = $heure + 1;
-    }
 }
 
+if ($minute > 60){
+    $minute = $minute - 60;
+    $heure = $heure + 1;
+}
 
+if ($heure >= 24){
+    $heure = $heure - 24;
+}
 
 echo "Dans une seconde, il sera : " . $heure . "h". $minute . "m" . $seconde
 
---
+++ PHP/3-LesStructuresdeControle/exo9.php
@@ -0,0 +1,16 @@
+<?php
+
+$age = readline("Quel est votre âge ?");
+$sexe = readline("Préciser le sexe");
+
+if ($sexe = "homme" && $age > 20){
+    echo "imposable";
+}
+elseif ($sexe = "femme" && $age > 18 && $age < 35){
+    echo "imposable";
+}
+else{
+    echo "non imposable";
+}
+
+?>
