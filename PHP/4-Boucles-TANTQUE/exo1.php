a7274e7f17d8c957ee54d182dac2cceb2207f396
DamienPalm <palm.damien@icloud.com>
2022-11-07 16:20:43 +0100

    maj 07/11/22

--- PHP/4-Boucles-TANTQUE/exo1.php
+++ PHP/4-Boucles-TANTQUE/exo1.php
@@ -2,10 +2,17 @@
 
 $nb = readline ("mettre un nombre entre 1 et 3 : ");
 
+
+if (filter_var ($nb, FILTER_VALIDATE_INT) !== false) {
+    echo($nb . " est pas un entier");
+}
+else {
+    echo( $nb . " n'est pas un entier");
+}
+
 while ($nb < 1 || $nb > 3){
     $nb = readline ("mettre un nombre entre 1 et 3 : ");
 }
-
 echo "GG";
 
 ?>
