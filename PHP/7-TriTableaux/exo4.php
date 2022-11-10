59a38cbf05fedd24fe873f01ab11ec41a1780dee
DamienPalm <palm.damien@gmail.com>
2022-11-10 23:47:08 +0100

    maj 10/11/22

--- PHP/7-TriTableaux/exo4.php
+++ PHP/7-TriTableaux/exo4.php
@@ -5,12 +5,12 @@ $saisie = readline ("Quel mot cherchez vous ? ");
 $estVrai = false;
 for ($i=0 ; $i < count ($dico) ; $i++){
     if ($dico[$i] == $saisie){
-        echo "Le mot est bien dans le dico";
+        echo $saisie . " est bien dans le dico";
         $estVrai = true;
     }
 }
 
 if ($estVrai == false){
-echo "n'est pas dans le dico";
+echo $saisie . " n'est pas dans le dico";
 }
 ?>
