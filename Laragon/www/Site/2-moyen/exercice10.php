f36417a52ed67728292bd5b9472aa2e38bc931b9
DamienPalm <palm.damien@icloud.com>
2022-11-29 16:17:26 +0100

    maj 29/11/22

---
+++ Laragon/www/Site/2-moyen/exercice10.php
@@ -0,0 +1,68 @@
+<?php ob_start() ?>
+
+
+<?php
+
+class Stagiaire{
+
+    private $nom;
+    private $note;
+
+    public function __construct($nom, $note){
+        $this->nom = $nom;
+        $this->note = $note;
+    }
+
+    //GETTER
+
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function getNote(){
+        return $this->note;
+    }
+
+    //SETTER
+
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+    public function setNote($note){
+        $this->note = $note;
+    }
+
+}
+
+$stagiaire1 = new Stagiaire ("Damien", [20, 18, 15, 17, 15]);
+$stagiaire2 = new Stagiaire ("Lucie", [15, 12, 13, 20, 14]);
+$stagiaire3 = new Stagiaire ("Florentin", [17, 18, 19, 15, 12, 16]);
+$stagiaire4 = new Stagiaire ("Fred", [20, 17, 16, 18, 15]);
+
+$stagiaires = [$stagiaire1, $stagiaire2, $stagiaire3, $stagiaire4];
+
+foreach($stagiaires as $stagiaire){
+    echo $stagiaire->getNom() . " ";
+    foreach($stagiaire->getNote() as $value){
+        echo $value . " ";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+<?php
+$content = ob_get_clean();
+$titre = "Exercice 3 POO - GETTER et SETTER";
+require "../template.php";
+?>
---
+++ Laragon/www/Site/2-moyen/exercice8.php
@@ -0,0 +1,99 @@
+<?php ob_start()?>
+
+<form action="" method="get">
+    <input type="submit" class="btn btn-primary" value="Animaux" name="Animaux"/>
+    <input type="submit" class="btn btn-primary" value="Chien" name="Chien"/>
+    <input type="submit" class="btn btn-primary" value="Chat" name="Chat">
+</form>
+
+<?php
+
+
+
+class Animal {
+    private $nom;
+    private $age;
+    private $type;
+
+    public function __construct($nom, $age, $type){
+        $this->nom = $nom;
+        $this->age = $age;
+        $this->type = $type;
+    }
+
+    //getter
+
+    public function getNom() {
+        return $this->nom;
+    }
+    public function getAge() {
+        return $this->age;
+    }
+    public function getType() {
+        return $this->type;
+    }
+
+    //setter
+
+    public function setNom ($nom){
+        $this->nom = $nom;
+        return $this->nom;
+    }
+    public function setAge ($age){
+        $this->age = $age;
+        return $this->age;
+    }
+    public function setType ($type){
+        $this->type = $type;
+        return $this->type;
+    }
+    
+}
+
+$animal1 = new Animal("Myla", 6, "chien");
+$animal2 = new Animal("Sparrow", 7, "chat");
+$animal3 = new Animal("Hina", 8, "chat");
+$animaux = [$animal1, $animal2, $animal3];
+
+foreach ($animaux as $animal){
+    $nom = $animal->getNom();
+    $age = $animal->getAge();
+    $type = $animal->getType();
+}
+
+?>
+
+<p>
+<?php
+
+if(isset($_GET["Animaux"])){
+    foreach($animaux as $animal){
+        if($type=$animal->getType()){
+            echo "Je m'appelle " . $animal->getNom() . " j'ai " . $animal->getAge() . " ans et je suis un " . $animal->getType() . "<br>";
+        }
+    }
+}
+
+if(isset($_GET["Chien"])){
+    foreach($animaux as $animal){
+        if($type=$animal->getType()=="chien"){
+            echo "Je m'appelle " . $animal->getNom() . " j'ai " . $animal->getAge() . " ans et je suis un " . $animal->getType() . "<br>";
+        }
+    }
+}
+
+if(isset($_GET["Chat"])){
+    foreach($animaux as $animal){
+        if($type=$animal->getType()=="chat"){
+            echo "Je m'appelle " . $animal->getNom() . " j'ai " . $animal->getAge() . " ans et je suis un " . $animal->getType() . "<br>";
+        }
+    }
+}
+?>
+</p>
+
+<?php
+$content = ob_get_clean();
+$titre = "Exercice 1 POO - GETTER et SETTER";
+require "../template.php";
+?>
